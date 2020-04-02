175d1ebb812949e00f0d98f992958122c76f53a5
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-04-02 15:48:33 +0200

    [ts] add tests for ArrayEList

--- ecore/tests/ArrayEList.test.ts
+++ ecore/tests/ArrayEList.test.ts
@@ -1,6 +1,6 @@
 import { ArrayEList } from '../src/ArrayEList'
 
-test('Get', () => {
+test('get', () => {
     var a = new ArrayEList<number>([1, 2, 3, 4]);
     expect(a.get(0)).toBe(1);
     expect(a.get(1)).toBe(2);
@@ -8,7 +8,7 @@ test('Get', () => {
     expect(a.get(3)).toBe(4);
 });
 
-test('Size', () => {
+test('size', () => {
     {
         var a = new ArrayEList<number>();
         expect(a.size()).toBe(0);
@@ -19,7 +19,7 @@ test('Size', () => {
     }
 });
 
-test('Empty', () => {
+test('isEmpty', () => {
     {
         var a = new ArrayEList<number>();
         expect(a.isEmpty()).toBeTruthy();
@@ -30,21 +30,36 @@ test('Empty', () => {
     }
 });
 
-test('Add',()=>{
+test('add',()=>{
     var a = new ArrayEList<number>();
     expect(a.add(0)).toBeTruthy();
     expect(a.add(1)).toBeTruthy();
     expect(a.toArray()).toEqual([0,1]);
 });
 
-test('Add_Unique',()=>{
+test('add_unique',()=>{
     var a = new ArrayEList<number>([],true);
     expect(a.add(0)).toBeTruthy();
     expect(a.add(0)).toBeFalsy();
     expect(a.toArray()).toEqual([0]);
 });
 
-test('Insert',()=>{
+
+test('addAll',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    var b = new ArrayEList<number>([3,4,5]);
+    expect( a.addAll(b)).toBeTruthy();
+    expect(a.toArray()).toEqual([0,1,2,3,4,5]);
+});
+
+test('addAll_unique',()=>{
+    var a = new ArrayEList<number>([0,1,2],true);
+    var b = new ArrayEList<number>([1,2,3]);
+    expect( a.addAll(b)).toBeTruthy();
+    expect(a.toArray()).toEqual([0,1,2,3]);
+});
+
+test('insert',()=>{
     {
         var a = new ArrayEList<number>();
         expect(a.insert(0,0)).toBeTruthy();
@@ -62,14 +77,104 @@ test('Insert',()=>{
     }
 });
 
-test('Insert_InvalidIndex',()=>{
+test('insert_invalid_range',()=>{
     var a = new ArrayEList<number>();
     expect(() => {a.insert(1,0)}).toThrowError(RangeError);
 });
 
-test('Insert_Unique',()=>{
+test('insert_unique',()=>{
     var a = new ArrayEList<number>([0,1,2],true);
     expect(a.insert(0,2)).toBeFalsy();
 });
 
+test('insertAll',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    var b = new ArrayEList<number>([3,4,5]);
+    expect( a.insertAll(0,b)).toBeTruthy();
+    expect(a.toArray()).toEqual([3,4,5,0,1,2]);
+});
+
+test('insertAll_unique',()=>{
+    var a = new ArrayEList<number>([0,1,2],true);
+    var b = new ArrayEList<number>([1,2,3]);
+    expect( a.insertAll(0,b)).toBeTruthy();
+    expect(a.toArray()).toEqual([3,0,1,2]);
+});
+
+test('remove',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    expect(a.remove(1)).toBeTruthy();
+    expect(a.toArray()).toEqual([0,2]);
+    expect(a.remove(1)).toBeFalsy();
+});
+
+
+test('removeAt',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    expect(a.removeAt(0)).toBe(0);
+    expect(a.toArray()).toEqual([1,2]);
+});
+
+test('removeAt_invalid_range',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    expect(() => {a.removeAt(3)}).toThrowError(RangeError);
+});
+
+test('removeAll',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    var b = new ArrayEList<number>([1,2]);
+    expect( a.removeAll(b) ).toBeTruthy();
+    expect(a.toArray()).toEqual([0]);
+});
+
+test('retainAll',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    var b = new ArrayEList<number>([1,2]);
+    expect( a.retainAll(b) ).toBeTruthy();
+    expect(a.toArray()).toEqual([1,2]);
+});
+
+test('set',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    expect(a.set(0,1)).toBe(0);
+    expect(a.toArray()).toEqual([1,1,2]);
+});
+
+test('set_invalid_range',()=>{
+    var a = new ArrayEList<number>([0,1,2]);
+    expect(() => {a.set(3,1)}).toThrowError(RangeError);
+});
+
+test('set_invalid_constraint',()=>{
+    var a = new ArrayEList<number>([0,1,2],true);
+    expect(() => {a.set(0,1)}).toThrow();
+});
+
+test('indexOf',() => {
+    var a = new ArrayEList<number>([0,1,2]);
+    expect(a.indexOf(1)).toBe(1);
+    expect(a.indexOf(3)).toBe(-1);
+});
+
+test('clear',() => {
+    var a = new ArrayEList<number>([0,1,2]);
+    expect(a.isEmpty()).toBeFalsy();
+    a.clear();
+    expect(a.isEmpty()).toBeTruthy();
+});
+
+test('contains',() => {
+    var a = new ArrayEList<number>([0,1,2]);
+    expect(a.contains(1)).toBeTruthy();
+    expect(a.contains(3)).toBeFalsy();
+});
+
+test('iterator',() => {
+    var a = new ArrayEList<number>([3,4,5]);
+    var v : number[] = [];
+    for (const i of a) {
+        v.push(i);
+    }
+    expect(v).toEqual([3,4,5]);
+});
 
