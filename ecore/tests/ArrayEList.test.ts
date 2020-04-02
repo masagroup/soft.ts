519100c9560cdcaa95429048aff88f3e7061396c
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-04-02 15:23:12 +0200

    [ts] add ArrayEList empty/add/insert tests

--- ecore/tests/ArrayEList.test.ts
+++ ecore/tests/ArrayEList.test.ts
@@ -19,3 +19,57 @@ test('Size', () => {
     }
 });
 
+test('Empty', () => {
+    {
+        var a = new ArrayEList<number>();
+        expect(a.isEmpty()).toBeTruthy();
+    }
+    {
+        var a = new ArrayEList<number>([1, 2, 3, 4]);
+        expect(a.isEmpty()).toBeFalsy();
+    }
+});
+
+test('Add',()=>{
+    var a = new ArrayEList<number>();
+    expect(a.add(0)).toBeTruthy();
+    expect(a.add(1)).toBeTruthy();
+    expect(a.toArray()).toEqual([0,1]);
+});
+
+test('Add_Unique',()=>{
+    var a = new ArrayEList<number>([],true);
+    expect(a.add(0)).toBeTruthy();
+    expect(a.add(0)).toBeFalsy();
+    expect(a.toArray()).toEqual([0]);
+});
+
+test('Insert',()=>{
+    {
+        var a = new ArrayEList<number>();
+        expect(a.insert(0,0)).toBeTruthy();
+        expect(a.toArray()).toEqual([0]);
+    }
+    {
+        var a = new ArrayEList<number>([1]);
+        expect(a.insert(0,0)).toBeTruthy();
+        expect(a.toArray()).toEqual([0,1]);
+    }
+    {
+        var a = new ArrayEList<number>([1]);
+        expect(a.insert(1,0)).toBeTruthy();
+        expect(a.toArray()).toEqual([1,0]);
+    }
+});
+
+test('Insert_InvalidIndex',()=>{
+    var a = new ArrayEList<number>();
+    expect(() => {a.insert(1,0)}).toThrowError(RangeError);
+});
+
+test('Insert_Unique',()=>{
+    var a = new ArrayEList<number>([0,1,2],true);
+    expect(a.insert(0,2)).toBeFalsy();
+});
+
+
