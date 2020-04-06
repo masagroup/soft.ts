358cf8edfaa69f0768b2a0cc40d0ca7bc4b509c8
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-04-06 10:50:12 +0200

    [ecore][ts] add EResource ts file

---
+++ ecore/src/EResource.ts
@@ -0,0 +1,12 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2020 MASA Group
+//
+// *****************************************************************************
+
+export interface EResource
+{
+}
