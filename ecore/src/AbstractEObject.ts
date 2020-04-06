27363861c779e6419b12485c72322b5262a9dad6
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-04-06 11:29:32 +0200

    [ecore][ts] add AbstractEObject class

---
+++ ecore/src/AbstractEObject.ts
@@ -0,0 +1,15 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2020 MASA Group
+//
+// *****************************************************************************
+
+import { AbstractNotifier } from "./AbstractNotifier";
+import { EObject } from "./EObject";
+
+export class AbstractEObject extends AbstractNotifier implements EObject {
+    
+}
