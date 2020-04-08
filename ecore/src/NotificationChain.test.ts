6b4fc598b22df51ac68217ba0b4a26c1433a6a7c
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-04-08 12:27:57 +0200

    [ecore][ts] define NotificationChain class

---
+++ ecore/src/NotificationChain.test.ts
@@ -0,0 +1,58 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2020 MASA Group
+//
+// *****************************************************************************
+import test from 'ava'
+import { NotificationChain } from "./NotificationChain";
+import { ENotifier } from "./ENotifier";
+import { ENotification, EventType } from "./ENotification";
+import { mock, instance, when, verify } from 'ts-mockito';
+
+test('constructor', t => {
+    var c = new NotificationChain();
+    t.not(c,null);
+});
+
+test('add', t => {
+    // chain
+    var c = new NotificationChain();
+
+    // mocks
+    const mockNotifier = mock<ENotifier>();
+    const mockNotification = mock<ENotification>();
+    const notifier = instance(mockNotifier);
+    const notification = instance(mockNotification);
+    
+    // when
+    when(mockNotification.eventType).thenReturn(EventType.ADD);
+    when(mockNotification.notifier).thenReturn(notifier);
+    when(mockNotification.merge(notification)).thenReturn(false);
+
+    // test
+    t.true( c.add(notification) );
+    t.true( c.add(notification) );
+});
+
+test('dispatch', t => {
+    // mocks
+    const mockNotifier = mock<ENotifier>();
+    const mockNotification = mock<ENotification>();
+    const notifier = instance(mockNotifier);
+    const notification = instance(mockNotification);
+
+    // when
+    when(mockNotification.eventType).thenReturn(EventType.ADD);
+    when(mockNotification.notifier).thenReturn(notifier);
+
+    // test
+    var c = new NotificationChain();
+    t.true( c.add(notification) );
+    c.dispatch();
+
+    // verifications
+    t.notThrows(() => verify(mockNotifier.eNotify(notification)).once());
+});
