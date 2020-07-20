894665ce6b27a9664a27a76779ae885d1690a5db
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-07-20 17:26:48 +0200

    [ts] define EObjectList interface

---
+++ ecore/src/EObjectList.ts
@@ -0,0 +1,15 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2020 MASA Group
+//
+// *****************************************************************************
+
+import { ENotifyingList } from "./ENotifyingList";
+import { EList } from "./EList";
+
+export interface EObjectList<E> extends ENotifyingList<E> {
+    getUnResolvedList() : EList<E>;
+}
