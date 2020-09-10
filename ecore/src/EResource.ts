bf46773e7e0bc5ce866a1e64067f846d5d8b5076
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2020-09-10 16:03:59 +0200

    [ecore][ts] define EResource interface

--- ecore/src/EResource.ts
+++ ecore/src/EResource.ts
@@ -7,4 +7,36 @@
 //
 // *****************************************************************************
 
-export interface EResource {}
+import { ENotifier } from "./ENotifier";
+import { EObject } from "./EObject";
+import { EList } from "./EList";
+import { EResourceSet } from "./EResourceSet";
+
+
+export class EResourceConstans {
+
+    public static readonly RESOURCE__RESOURCE_SET : number = 0;
+
+    public static readonly RESOURCE__URI : number = 1;
+
+    public static readonly RESOURCE__CONTENTS : number = 2;
+
+    public static readonly RESOURCE__IS_LOADED : number = 4;
+}
+
+export interface EResource extends ENotifier {
+
+    eURI : URL;
+
+    eResourceSet() : EResourceSet;
+    eContents() : EList<EObject>;
+    eAllContents() : IterableIterator<EObject>;
+    
+    load() : void;
+    loadFromString() : void;
+    unload() : void;
+    readonly isLoaded : boolean;
+
+    save() : void;
+    saveToString() : void;
+}
