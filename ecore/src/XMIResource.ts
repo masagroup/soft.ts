3393b453b7a95e3147cf7dc6492e3e75df303e88
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-09-14 17:19:28 +0200

    [ecore][ts] define XMIResource class

---
+++ ecore/src/XMIResource.ts
@@ -0,0 +1,38 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2020 MASA Group
+//
+// *****************************************************************************
+
+import { XMLResource, XMLLoad, XMLSave } from "./XMLResource";
+
+export class XMIResource extends XMLResource {
+
+    version : string = "";
+
+    protected createLoad() : XMLLoad {
+        return new XMILoad(this);
+    }
+
+    protected createSave() : XMLSave {
+        return new XMISave(this);
+    }
+}
+
+export class XMILoad extends XMLLoad {
+
+    constructor(resource : XMLResource) {
+        super(resource);
+    }
+}
+
+export class XMISave extends XMLSave {
+
+    constructor(resource : XMLResource) {
+        super(resource);
+    }
+
+}
