83d36a1894a1d463263f3f23e2a3fce529cf008f
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-09-15 12:48:10 +0200

    [ecore][ts] FileURIHandler class implementation

---
+++ ecore/src/FileURIHandler.test.ts
@@ -0,0 +1,31 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2020 MASA Group
+//
+// *****************************************************************************
+import test from "ava";
+import { FileURIHandler } from "./FileURIHandler";
+
+test('canHandle', t => {
+   
+    let uriHandler = new FileURIHandler();
+
+    t.true( uriHandler.canHandle(new URL("file://test")));
+    t.false( uriHandler.canHandle(new URL("http://test")));
+});
+
+test('createReadStream', t => {
+    
+    let uriHandler = new FileURIHandler();
+    let s = uriHandler.createReadStream( new URL("file:///" + __dirname + "/../testdata/read-stream.txt"));
+    t.true(s != null);
+});
+
+test('createWriteStream', t => {
+    let uriHandler = new FileURIHandler();
+    let s = uriHandler.createWriteStream( new URL("file:///" + __dirname + "/../testdata/write-stream.txt"));
+    t.true(s != null);
+});
---
+++ ecore/src/FileURIHandler.ts
@@ -0,0 +1,27 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2020 MASA Group
+//
+// *****************************************************************************
+
+import * as fs from "fs";
+import { EURIHandler } from "./EURIHandler";
+
+export class FileURIHandler implements EURIHandler {
+
+    canHandle(uri: URL): boolean {
+        return uri.protocol == "file:" || (uri.protocol == null && uri.host == null && uri.search == null );
+    }
+
+    createReadStream(uri: URL): fs.ReadStream {
+        return fs.createReadStream( uri );
+    }
+
+    createWriteStream(uri: URL): fs.WriteStream {
+        return fs.createWriteStream( uri );
+    }
+
+}
