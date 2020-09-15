bc7cf871ee5612d56ded2900ce643d7ca43f981e
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-09-15 10:43:08 +0200

    [ecore][ts] import sax parser and create a parser in XMLLoad

--- ecore/src/XMIResource.ts
+++ ecore/src/XMIResource.ts
@@ -10,29 +10,25 @@
 import { XMLResource, XMLLoad, XMLSave } from "./XMLResource";
 
 export class XMIResource extends XMLResource {
+    version: string = "";
 
-    version : string = "";
-
-    protected createLoad() : XMLLoad {
+    protected createLoad(): XMLLoad {
         return new XMILoad(this);
     }
 
-    protected createSave() : XMLSave {
+    protected createSave(): XMLSave {
         return new XMISave(this);
     }
 }
 
 export class XMILoad extends XMLLoad {
-
-    constructor(resource : XMLResource) {
+    constructor(resource: XMLResource) {
         super(resource);
     }
 }
 
 export class XMISave extends XMLSave {
-
-    constructor(resource : XMLResource) {
+    constructor(resource: XMLResource) {
         super(resource);
     }
-
 }
--- ecore/src/XMLResource.ts
+++ ecore/src/XMLResource.ts
@@ -8,54 +8,62 @@
 // *****************************************************************************
 
 import * as fs from "fs";
+import * as sax from "sax";
 import { EResourceImpl } from "./EResourceImpl";
 
 export class XMLResource extends EResourceImpl {
-
     protected doLoad(rs: fs.ReadStream): void {
         let l = this.createLoad();
-        l.load( rs );
+        l.load(rs);
     }
-
 
     protected doSave(ws: fs.WriteStream): void {
         let s = this.createSave();
-        s.save( ws );
+        s.save(ws);
     }
 
-    protected createLoad() : XMLLoad {
+    protected createLoad(): XMLLoad {
         return new XMLLoad(this);
     }
 
-    protected createSave() : XMLSave {
+    protected createSave(): XMLSave {
         return new XMLSave(this);
     }
 }
 
 export class XMLLoad {
+    private _resource: XMLResource;
+    private _isResolvedDefered : boolean = false;
+    private _elements :string[];
 
-    private _resource : XMLResource;
 
-    constructor(resource : XMLResource) {
+    constructor(resource: XMLResource) {
         this._resource = resource;
     }
 
-    load( rs: fs.ReadStream ) : void {
-        
+    load(rs: fs.ReadStream): void {
+        let saxStream = new sax.SAXStream(true,{trim:true,lowercase:true,xmlns:true,position:true,});
+        saxStream.on("opentag",this.startTag);
+        saxStream.on("closetag",this.endTag);
+
+        rs.pipe(saxStream);
     }
 
+    startTag( tag : sax.Tag | sax.QualifiedTag ) {
+
+    }
+
+    endTag( tagName : string ) {
+
+    }
 }
 
 export class XMLSave {
+    private _resource: XMLResource;
 
-    private _resource : XMLResource;
-
-    constructor(resource : XMLResource) {
+    constructor(resource: XMLResource) {
         this._resource = resource;
     }
 
-    save( rs: fs.WriteStream) : void {
-        
-    }
-
+    save(rs: fs.WriteStream): void {}
 }
