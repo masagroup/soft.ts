099f9067b9b7579c4e92583c0fc7dc78fd56c830
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2020-09-17 15:39:46 +0200

    [ecore][ts] EResourceSetImpl test adding resources

--- ecore/src/EResourceSetImpl.test.ts
+++ ecore/src/EResourceSetImpl.test.ts
@@ -8,9 +8,29 @@
 // *****************************************************************************
 
 import test from "ava";
+import { instance, mock, verify, when } from "ts-mockito";
+import { EResourceImpl } from "./EResourceImpl";
+import { EResourceInternal } from "./EResourceInternal";
 import { EResourceSetImpl } from "./EResourceSetImpl";
 
 test("constructor", (t) => {
     let rs = new EResourceSetImpl();
     t.true( rs.getURIResourceMap() == null );
 });
+
+test("resourcesWithMock", (t) => {
+    let rs = new EResourceSetImpl();
+    let mockEResource = mock<EResourceInternal>();
+    let eResource = instance(mockEResource);
+    when(mockEResource.basicSetResourceSet(rs,null)).thenReturn(null);
+    t.true( rs.getResources().add(eResource) );
+});
+
+test("resourcesNoMock", (t) => {
+    let rs = new EResourceSetImpl();
+    let r = new EResourceImpl();
+
+    rs.getResources().add(r);
+    t.is(r.eResourceSet() , rs ); 
+});
+
