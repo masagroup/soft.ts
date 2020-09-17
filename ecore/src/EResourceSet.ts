240e07fab9934bb15556982c6bc99c63e2943c9c
Jocelyn DUMAY <jocelyn.dumay@masagroup.net>
2020-09-17 11:28:39 +0200

    [ecore][ts] add EResourceSetConstant class

--- ecore/src/EResourceSet.ts
+++ ecore/src/EResourceSet.ts
@@ -13,6 +13,10 @@ import { EList } from "./EList";
 import { EObject } from "./EObject";
 import { EURIConverter } from "./EURIConverter";
 
+export class EResourceSetConstants {
+    public static readonly RESOURCE_SET__RESOURCES: number = 0;
+}
+
 export interface EResourceSet extends ENotifier {
     getResources(): EList<EResource>;
     getResource(uri: URL, loadOnDemand: boolean): EResource;
