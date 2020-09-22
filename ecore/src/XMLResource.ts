125de392582ead2d978213f2dfad2d9e7432b928
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-09-22 16:14:10 +0200

    [ecore][ts] define XmlNamespace class in XMLResource

--- ecore/src/XMLResource.ts
+++ ecore/src/XMLResource.ts
@@ -11,6 +11,66 @@ import * as fs from "fs";
 import * as sax from "sax";
 import { EResourceImpl } from "./EResourceImpl";
 
+export class XMLNamespaces {
+    private _namespaces: { prefix: string; uri: string }[] = [];
+    private _namespacesSize: number = 0;
+    private _currentContext: number = -1;
+    private _contexts: number[] = [];
+
+    pushContext(): void {
+        this._currentContext++;
+        if (this._currentContext >= this._contexts.length) {
+            this._contexts.push(this._namespacesSize);
+        } else {
+            this._contexts[this._currentContext] = this._namespacesSize;
+        }
+    }
+
+    popContext(): { prefix: string; uri: string }[] {
+        let oldPrefixSize = this._namespacesSize;
+        this._namespacesSize = this._contexts[this._currentContext];
+        this._currentContext--;
+        return this._namespaces.slice(this._namespacesSize, oldPrefixSize);
+    }
+
+    declarePrefix(prefix: string, uri: string): boolean {
+        for (let i = this._namespacesSize; i > this._contexts[this._currentContext]; i--) {
+            let p = this._namespaces[i - 1];
+            if (p.prefix == prefix) {
+                p.uri = uri;
+                return true;
+            }
+        }
+        this._namespacesSize++;
+        if (this._namespacesSize > this._namespaces.length) {
+            this._namespaces.push({ prefix: prefix, uri: uri });
+        } else {
+            this._namespaces[this._namespacesSize] = { prefix: prefix, uri: uri };
+        }
+        return false;
+    }
+
+    getPrefix(uri: string): string {
+        for (let i = this._namespacesSize; i > 0; i--) {
+            let p = this._namespaces[i - 1];
+            if (p.uri == uri) {
+                return p.prefix;
+            }
+        }
+        return null;
+    }
+
+    getURI(prefix: string): string {
+        for (let i = this._namespacesSize; i > 0; i--) {
+            let p = this._namespaces[i - 1];
+            if (p.prefix == prefix) {
+                return p.uri;
+            }
+        }
+        return null;
+    }
+}
+
 export class XMLResource extends EResourceImpl {
     protected doLoad(rs: fs.ReadStream): void {
         let l = this.createLoad();
@@ -53,7 +113,7 @@ export class XMLLoad {
         rs.pipe(saxStream);
     }
 
-    onStartTag(tag: sax.Tag | sax.QualifiedTag) {}
+    onStartTag(tag: sax.QualifiedTag) {}
 
     onEndTag(tagName: string) {}
 
