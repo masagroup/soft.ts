2c45e35340a65e26e0c92cd304ca03d45f07819f
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-09-23 18:21:05 +0200

    [ecore][ts] EPackageExt::getClassifier implementation

--- ecore/src/EPackageExt.ts
+++ ecore/src/EPackageExt.ts
@@ -1,3 +1,8 @@
+import { Adapter } from "./Adapter";
+import { EAdapter } from "./EAdapter";
+import { EClassifier } from "./EClassifier";
+import { EcoreConstants } from "./EcoreConstants";
+import { ENotification, EventType } from "./ENotification";
 // *****************************************************************************
 //
 // This file is part of a MASA library or program.
@@ -9,8 +14,38 @@
 
 import { EPackageImpl } from "./EPackageImpl";
 
-export class EPackageExt extends EPackageImpl {
-    constructor() {
+class EPackageExtAdapter extends Adapter {
+    constructor ( private _pack : EPackageExt ) {
         super();
     }
+
+    notifyChanged(notification: ENotification): void {
+        if ( notification.eventType != EventType.REMOVING_ADAPTER && notification.featureID == EcoreConstants.EPACKAGE__ECLASSIFIERS) {
+            this._pack._nameToClassifier = null;
+        }
+    }
+
+}
+
+export class EPackageExt extends EPackageImpl {
+    _nameToClassifier : Map<string,EClassifier> = null;
+    _adapter : EAdapter = null;
+
+    constructor() {
+        super();
+        this._adapter = new EPackageExtAdapter(this);
+        this.eAdapters.add(this._adapter);
+    }
+
+    getEClassifier(name : string) : EClassifier {
+        if ( this._nameToClassifier == null ) {
+            this._nameToClassifier = new Map<string,EClassifier>();
+            for (const classifier of this.eClassifiers) {
+                this._nameToClassifier.set(classifier.name, classifier);
+            }
+        }
+	    return this._nameToClassifier.get(name);
+	}
+
+
 }
