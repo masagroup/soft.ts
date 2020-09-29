ecee66ec61d91698f1236a7e849c95ccd6fa1cb6
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-09-29 09:18:51 +0200

    [ecore][ts] ensure that uri is well defined

--- ecore/src/XMLResource.ts
+++ ecore/src/XMLResource.ts
@@ -496,8 +496,8 @@ export class XMLLoad {
         (eProxy as EObjectInternal).eSetProxyURI(uri);
 
         let ndx = id.indexOf("#");
-        let trimmedURI: string = ndx != -1 ? id.slice(0, ndx - 1) : id;
-        if (this._resource.eURI.toString() == trimmedURI) {
+        let trimmedURI: string = ndx != -1 ? (ndx > 0 ? id.slice(0, ndx - 1) : "" ): id;
+        if (this._resource.eURI?.toString() == trimmedURI) {
             this._sameDocumentProxies.push(eProxy);
         }
     }
