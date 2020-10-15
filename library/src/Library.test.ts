fb6d89eac7793937077031a49ea2ffbe370b2d2f
JDY-PC\Jocelyn <jocelyn@barry-dumay.com>
2020-10-15 17:14:51 +0200

    [library][ts] test load/save symetry

--- library/src/Library.test.ts
+++ library/src/Library.test.ts
@@ -8,6 +8,7 @@
 // *****************************************************************************
 
 import * as ecore from "@masagroup/ecore"
+import * as fs from "fs";
 import { getLibraryPackage } from "./internal";
 
 describe("Library",() => {
@@ -22,6 +23,26 @@ describe("Library",() => {
         expect(resource.isLoaded).toBeTruthy();
         expect(resource.getErrors().isEmpty()).toBeTruthy();
         expect(resource.getWarnings().isEmpty()).toBeTruthy();
+    });
+
+    test("save", () => {
+
+        ecore.getPackageRegistry().registerPackage(getLibraryPackage());
+        // create resource
+        let fileURI = new URL("file:///" + __dirname + "/../testdata/library.xml");
+        let resourceFactory = ecore.getResourceFactoryRegistry().getFactory(fileURI);
+        let resource = resourceFactory.createResource(fileURI);
+        expect(resource).not.toBeNull();
+        
+        // load file content to string
+        let content = fs.readFileSync(resource.eURI).toString().replace(/\r?\n|\r/g, "\n");          
+        
+        // load resource
+        resource.loadFromString( content );
+
+        // check is load/save are symetric
+        let result = resource.saveToString();
+        expect(result).toBe( content );
     });
 
 });
