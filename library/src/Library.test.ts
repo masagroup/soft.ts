70abbfc901b14aa245913488b6e82d44ef3c7183
JDY <jocelyn@barry-dumay.com>
2021-06-01 19:22:35 +0200

    [library][ts] library load/save test with complex file

--- library/src/Library.test.ts
+++ library/src/Library.test.ts
@@ -51,7 +51,7 @@ describe("Library",() => {
         expect(result).toBe(expected);
     });
 
-    test("save.complex",() => {
+    test("save.complex", async () => {
         let xmlProcessor = new ecore.XMLProcessor([getLibraryPackage()]);
         let fileURI = new URL("file:///" + __dirname + "/../testdata/library.complex.xml");
         let resource = xmlProcessor.loadSync(fileURI);
@@ -64,25 +64,5 @@ describe("Library",() => {
         .replace(/\r?\n|\r/g, "\n");
         expect(result).toBe(expected);
     });
-
-    // test("save", () => {
-
-    //     ecore.getPackageRegistry().registerPackage(getLibraryPackage());
-    //     // create resource
-    //     let fileURI = new URL("file:///" + __dirname + "/../testdata/library.xml");
-    //     let resourceFactory = ecore.getResourceFactoryRegistry().getFactory(fileURI);
-    //     let resource = resourceFactory.createResource(fileURI);
-    //     expect(resource).not.toBeNull();
-        
-    //     // load file content to string
-    //     let content = fs.readFileSync(resource.eURI).toString().replace(/\r?\n|\r/g, "\n");          
-        
-    //     // load resource
-    //     resource.loadFromString( content );
-
-    //     // check is load/save are symetric
-    //     let result = resource.saveToString();
-    //     expect(result).toBe( content );
-    // });
 
 });
