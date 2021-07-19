5207f7d54315c365b778bcd22aba375ee97929b5
JDY <jocelyn@barry-dumay.com>
2021-07-19 10:45:54 +0200

    [ecore][ts] add EDiagnosticImpl tests

---
+++ ecore/src/EDiagnosticImpl.test.ts
@@ -0,0 +1,20 @@
+// *****************************************************************************
+// Copyright(c) 2021 MASA Group
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// *****************************************************************************
+
+import { EDiagnosticImpl } from "./internal";
+
+describe("EDiagnosticImpl", () => {
+    test("accessors", () => {
+        let d = new EDiagnosticImpl("message", "location", 1, 2);
+        expect(d.message).toEqual("message");
+        expect(d.location).toEqual("location");
+        expect(d.line).toEqual(1);
+        expect(d.column).toEqual(2);
+    });
+});
