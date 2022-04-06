1d7ff49644d701e5d69625b135f58a428a8cb943
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2022-04-06 18:24:22 +0200

    [ecore][ts] add NoCodec tests

---
+++ ecore/src/NoCodec.test.ts
@@ -0,0 +1,17 @@
+import { NoCodec } from "./NoCodec"
+
+
+describe("NoCoder",()=>{
+    test("newEncoder", () => {
+        let codec = new NoCodec();
+        let e = codec.newEncoder(null);
+        expect(e).toBeNull();
+    })
+
+
+    test("newDecoder", () => {
+        let codec = new NoCodec();
+        let d = codec.newDecoder(null);
+        expect(d).toBeNull();
+    })
+})
