f73e39d5576d1ae6bd3111cbfa73f57af21f4af9
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2024-01-19 11:43:04 +0100

    [ecore][ts] add URI class

---
+++ ecore/src/URI.test.ts
@@ -0,0 +1,29 @@
+import {URI} from "./URI"
+
+describe('URI', () => {
+    test('constructor', () => {
+        let uri = new URI("file:///path#fragment")
+        expect(uri.protocol).toBe("file:")
+        expect(uri.host).toBe("")
+        expect(uri.port).toBe("")
+        expect(uri.pathname).toBe("/path")
+        expect(uri.hash).toBe("#fragment")
+    });
+
+    test('normalize', () => {
+        expect( new URI("http://host:10020").normalize() ).toEqual(new URI("http://host:10020"))
+        expect( new URI("http://host:10020/path").normalize() ).toEqual(new URI("http://host:10020/path"))
+        expect( new URI("http://host:10020/./path").normalize() ).toEqual(new URI("http://host:10020/path"))
+        expect( new URI("http://host:10020/path/../path2").normalize() ).toEqual(new URI("http://host:10020/path2"))
+        expect( new URI("http://host:10020/path/./path2").normalize() ).toEqual(new URI("http://host:10020/path/path2"))
+    });
+
+    test('authority', () => {
+        expect(new URI("http:///file.text").authority()).toEqual("")
+        expect(new URI("http:/file.text").authority()).toEqual("")
+        expect(new URI("http://host/file.text").authority()).toEqual("host")
+        expect(new URI("http://host:10/file.text").authority()).toEqual("host:10")
+        expect(new URI("http://userinfo@host:10/file.text").authority()).toEqual("userinfo@host:10")
+    });
+
+});
