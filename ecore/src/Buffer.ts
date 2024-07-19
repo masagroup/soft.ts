c17bd4db5369c93fdcd90ec6dabd586b437943ae
JDY <jocelyn.dumay@masagroup.net>
2024-07-19 11:09:20 +0200

    [ecore][ts] add comments

--- ecore/src/Buffer.ts
+++ ecore/src/Buffer.ts
@@ -1,4 +1,7 @@
 
+// The living standard of whatwg streams says
+// ReadableStream is also AsyncIterable, but
+// no browser implements it.
 export type ReadableStreamLike<T> = AsyncIterable<T> | ReadableStream<T>;
 
 export type BufferLike = ArrayLike<number> | Uint8Array | ArrayBufferView | ArrayBuffer
