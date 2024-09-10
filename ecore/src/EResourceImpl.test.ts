2600fce9fd002b01d6b1ffecb8d65b8b3e16a325
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2024-09-10 15:21:19 +0200

    [ecore] add EResourceImpl listeners tests

---
+++ ecore/src/EResourceImpl.test.ts
@@ -0,0 +1,23 @@
+import { anything, capture, instance, mock, verify, when } from "ts-mockito"
+import { describe, expect, test } from "vitest"
+import { EObject, EObjectList, EResourceImpl, EResourceListener, ImmutableEList } from "./internal.js";
+
+describe('EResourceImpl', () => {
+    test('listeners', () => {
+        const r = new EResourceImpl()
+        const mockObject = mock<EObject>()
+        const object = instance(mockObject)
+        const mockListener = mock<EResourceListener>()
+        const listener = instance(mockListener)
+        const mockObjectList = mock<EObjectList<EObject>>()
+        const objectList = instance(mockObjectList)
+        r.getResourceListeners().add(listener)
+
+        when(mockObject.eContents()).thenReturn(objectList)
+        when(mockObjectList.getUnResolvedList()).thenReturn(new ImmutableEList())
+        r.attached(object)
+        verify(mockListener.attached(object)).once()
+        r.detached(object)
+        verify(mockListener.detached(object)).once()
+    });
+})
