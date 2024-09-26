4378564fcfdcee0f386c70da4ac394ac552bf32b
Jocelyn DUMAY <jocelyn@barry-dumay.com>
2024-09-26 16:22:23 +0200

    [ecore] test isEList function

--- ecore/src/EList.test.ts
+++ ecore/src/EList.test.ts
@@ -1,15 +1,15 @@
-import { describe, expect, test } from "vitest";
-import { EList, isEList } from "./EList.js";
-import { anyNumber, instance, mock, when } from "ts-mockito";
+import { anyNumber, instance, mock, when } from "ts-mockito"
+import { describe, expect, test } from "vitest"
+import { EList, isEList } from "./EList.js"
 
-describe('EList', () => {
-    test('isEList', () => {
-        var a : any
+describe("EList", () => {
+    test("isEList", () => {
+        var a: any
         expect(isEList(a)).toBeFalsy()
 
         const mockList = mock<EList<any>>()
         const list = instance(mockList)
-        when(mockList.moveTo(anyNumber(),anyNumber())).thenReturn(undefined)
+        when(mockList.moveTo(anyNumber(), anyNumber())).thenReturn(undefined)
         expect(isEList(list)).toBeTruthy()
-    });
-});
+    })
+})
